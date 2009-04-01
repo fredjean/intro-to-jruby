9521bbed3056e00a7dc88c4a5fbe15fed06fe5b4
Frederic Jean <frederic.jean@sun.com>
2009-04-01 00:18:33 -0600

    Making slow progress.

---
+++ code/ruby_type.rb
@@ -0,0 +1,7 @@
+"hello".class # => String
+1.class       # => Fixnum
+10.3.class    # => Float
+[].class      # => Array
+{}.class      # => Hash
+Kernel.class  # => Module
+Object.class  # => Class
